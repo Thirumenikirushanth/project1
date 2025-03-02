9ac0f695d85e264fad1aa0a8e20d6df6563bc58a
Thirumenikirushanth <kirushanth0906@gmail.com>
2025-03-02 20:03:51 +0530

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+export const Navbar = () => {
+  return (
+    <nav className='container'>
+      <img src={logo} alt="" className='logo'/>
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li><button className='btn'>Contact us</button></li>
+      </ul>
+    </nav>
+      
+
+  )
+}
+
+
