d56d073a86a44871f68a2a81d0b5251fe5024cd3
Thirumenikirushanth <kirushanth0906@gmail.com>
2025-03-06 09:23:04 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Title from './components/Title/Title'
 import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 const App=()=>{
   return (
     <div>
@@ -19,6 +21,9 @@ const App=()=>{
         <Campus/>
         <Title subTitle='TESTIMONIALS' title='What Student Says'/>
         <Testimonials/>
+        <Title subTitle='Contact Us' title='Get in Touch'/>
+        <Contact/>
+        <Footer/>
       </div>
       
 
